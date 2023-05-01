b91e8275979f3830a019d0922f4f20dade99e3f8
Ivaylo Iliev <iivaylov00@gmail.com>
2023-05-01 22:52:02 +0300

    add project

---
+++ photogenic/src/main/java/com/photocontest/photogenic/PhotogenicApplication.java
@@ -0,0 +1,13 @@
+package com.photocontest.photogenic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotogenicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotogenicApplication.class, args);
+    }
+
+}
---
+++ photogenic/src/test/java/com/photocontest/photogenic/PhotogenicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.photocontest.photogenic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotogenicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
